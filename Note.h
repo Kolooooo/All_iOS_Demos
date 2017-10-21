e42451bc5d4a966c1e4c8636229f493547c53b95
Ken <13542502204@163.com>
2017-10-21 20:59:18 +0800

    NEW : Add the cocoapods ；

--- Note.h
+++ Note.h
@@ -1,6 +1,21 @@
 
 /**
- 
- * pod install --verbose --no-repo-update 更新/安装第三方库
- 
+ * Initialize the cocoapods to the target.
+ *
+ * 1. open the terminal 
+ * 2. $ cd #targetName#.xcodeproj
+ * 3. $ vim Podfile
+ * 4. $ i (Edit)
+        platform :ios, '8.0'
+        target '#targetName#' do
+            pod 'AFNetworking'
+        end
+ *
+ * 5. $ pod install
+ *
+ *
+ * Other method
+ *
+ * pod install --verbose --no-repo-update (update / initialize librarys)
+ *
  */
---
+++ Podfile
@@ -0,0 +1,14 @@
+platform :ios, '8.0'
+
+
+target 'iOS_ALL_Demo' do
+    
+  pod 'AFNetworking'
+  pod 'FMDB'
+  pod 'Masonry'
+  pod 'SDWebImage'
+  pod 'MJExtension'
+  pod 'MJRefresh'
+  pod 'SVProgressHUD'
+  
+end
