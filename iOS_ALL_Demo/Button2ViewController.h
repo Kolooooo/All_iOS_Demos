99465ea233a91a282cc4b749fb2cfcdea70d1995
Ken <13542502204@163.com>
2018-01-04 08:00:04 +0800

    添加长按录音 demo

---
+++ iOS_ALL_Demo/Button2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Button2ViewController.h
+//  iOS_ALL_Demo
+//
+//  Created by Ken_lu on 2018/1/4.
+//  Copyright © 2018年 Ken lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Button2ViewController : UIViewController
+
+@end
